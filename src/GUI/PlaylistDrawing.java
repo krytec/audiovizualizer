ff3aa8308f2ac850577c00111bb274fa98bbf4bb
krytec <flo.ortmann@web.de>
2017-12-14 10:47:24 +0100

    Started experimenting

--- src/GUI/PlaylistDrawing.java
+++ src/GUI/PlaylistDrawing.java
@@ -49,53 +49,68 @@ public class PlaylistDrawing {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 gc.setFill(Color.BLACK);
-                gc.fillRect(0,0,gc.getCanvas().getWidth(),gc.getCanvas().getHeight());
+                gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
                 gc.setStroke(Color.WHITE);
-                fft= new FFT(controller.getAudio().bufferSize(),controller.getAudio().sampleRate());
+                fft = new FFT(controller.getAudio().bufferSize(), controller.getAudio().sampleRate());
                 beatDetect = new BeatDetect();
                 beatDetect.detect(controller.getAudio().mix);
                 fft.forward(controller.getAudio().mix);
-                System.out.println(fft.specSize());
-                double radius = 200;
-                double points = fft.specSize()/3;
-                double slice = 2*Math.PI/points;
-                System.out.println("bs: "+(controller.getAudio().bufferSize()-1));
 
-                for (int i =0;i<points;i++) {
+
+                double radius = 200;
+                double points = fft.specSize() / 3;
+                double slice = 2 * Math.PI / points;
+
+
+               for (int i = 0; i < points; i++) {
                     gc.setStroke(Color.WHITE);
                     double angle = slice * i;
-                    double buffer = fft.getBand(i)*8;
-                    if(buffer<1){
-                        buffer*=2;
+                    double buffer = fft.getBand(i) * 8;
+                    if (buffer < 1) {
+                        buffer *= 2;
                     }
-                    if(buffer>3){
-                        buffer=buffer*1/3;
+                    if (buffer > 3) {
+                        buffer = buffer * 1 / 3;
                     }
                     double x = midx + radius * Math.cos(angle);
                     double y = midy + radius * Math.sin(angle);
-                    double newX =x+(buffer*Math.cos(angle));
-                    double newY =y+(buffer*Math.sin(angle));
+                    double newX = x + (buffer * Math.cos(angle));
+                    double newY = y + (buffer * Math.sin(angle));
 
 
+                    System.out.println("midx: " + midx);
+                    System.out.println("midy: " + midy);
+                    System.out.println("Buffer: " + Math.abs(fft.getBand(i)));
+                    System.out.println("y: " + y);
+                    System.out.println("x: " + x);
 
-
-
-                    System.out.println("midx: "+midx);
-                    System.out.println("midy: "+midy);
-                    System.out.println("Buffer: "+Math.abs(fft.getBand(i)));
-                    System.out.println("y: "+y);
-                    System.out.println("x: "+x);
-                    gc.strokeLine(x,y,newX,newY);
-
+                    gc.strokeLine(x, y, newX, newY);
+                    gc.strokeOval(newX, newY, 2, 2);
 
 
                 }
-                for (int i=0;i< controller.getAudio().sampleRate()-1;i++){
-                    gc.strokeLine(midx/2+i,midy+400+controller.getAudio().mix.get(i)*20,midx/2+i+1,midy+400+controller.getAudio().mix.get(i+1)*20);
-                }
+         /**       for (int i = 1; i < controller.getAudio().sampleRate()-1; i++) {
+                    gc.strokeLine(midx / 2 + i, midy + 400 + controller.getAudio().mix.get(i-1) * 20, midx / 2 + i + 1, midy + 400 + controller.getAudio().mix.get(i ) * 20);
+                } **/
 
+                for (int i = 0; i < 513; i++) {
+                    gc.setStroke(Color.WHITE);
+                    double angle = 2*Math.PI/513 * i;
+                    double buffer=0;
+                    for (int j=0;j<fft.specSize();j++) {
+                        buffer += fft.getBand(i) * 8;
+                    }
+                    buffer = buffer/fft.specSize();
+                    System.out.println(buffer);
+                    double x = midx + radius * Math.cos(angle);
+                    double y = midy + radius * Math.sin(angle);
+                    double newX = x + (buffer * Math.cos(angle));
+                    double newY = y + (buffer * Math.sin(angle));
+                    gc.setFill(Color.rgb(255,((buffer>255) ?(int) buffer/255 : (int)buffer),255));
+                    gc.fill();
+                    gc.fillOval(newX,newY,(buffer>40) ? buffer/40 : buffer, (buffer>40) ? buffer/40 : buffer);
+                }
             }
-
         });
 
         controller.trackProperty().addListener(new ChangeListener<Track>() {
